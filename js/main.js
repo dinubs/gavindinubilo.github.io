11081546ac0306ab56d6c21265065a0f8623c635
Gavin Dinubilo <gavin_dinubilo@mail.tmcc.edu>
2014-09-08 10:58:54 -0700

    Navbar JS

--- js/main.js
+++ js/main.js
@@ -35,9 +35,9 @@ $(function() {
 	var top = bar.css('top');
 	$(window).scroll(function() {
 		if($(this).scrollTop() > 200) {
-			bar.stop().animate({'background' : 'white'}, 500);
+			bar.stop().animate({'background' : 'rgba(255,255,255,0.65)'}, 500);
 		} else {
-			bar.stop().animate({'background' : 'clear'}, 500);
+			bar.stop().animate({'background' : 'transparent'}, 500);
 		}
 	});
 });
