c905e9b3e8bdb8456b3ce60ac306138c24f4a056
Gavin Dinubilo <gavin_dinubilo@mail.tmcc.edu>
2014-09-08 11:30:17 -0700

    JS

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({
-          scrollTop: target.offset().top - 34
+          scrollTop: target.offset().top - 30
         }, 1000);
         return false;
       }
